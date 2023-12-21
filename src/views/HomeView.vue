6e7bd929505987d1e08ea4e394f07886c28d860e
Mizu <psdk55052004@gmail.com>
2023-12-21 11:18:59 +0800

    新增 LINE 登入

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <GoogleLogin />
+  <LineLogin />
 </template>
 
 <script setup>
 import GoogleLogin from '@/components/GoogleLogin.vue'
+import LineLogin from '@/components/LineLogin.vue'
 </script>
 
 <style lang='scss' scope></style>

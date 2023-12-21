097468a2d49d3271c30ab284222b456d2822b5eb
Mizu <psdk55052004@gmail.com>
2023-12-21 16:32:03 +0800

    LINE: 授權應用程式權限

--- src/components/LineLogin.vue
+++ src/components/LineLogin.vue
@@ -3,17 +3,28 @@
        style="transform:translate(150px,-120px);
               padding:50px 40px;">
     <h2 class="text-30 text-center fw-bold-7 mb-15">Line 登入</h2>
+    <!-- 按鈕 -->
     <div class="btn line-logo"
          @click='lineLogin'>
+    </div>
+    <div class="text-center">
+      <button type='button'
+              class='btn btn-primary'
+              @click='accessApp'>
+        授權應用程式權限
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue' // eslint-disable-line
+import axios from 'axios' // eslint-disable-line
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
 // yourClient_id 請設置為實際Channel ID 值
-const clientId = import.meta.env.VITE_APP_CHANNEL_ID
+const channelId = import.meta.env.VITE_APP_CHANNEL_ID
 
 console.log(`import.meta.env.VITE_APP_MODE: ${import.meta.env.VITE_APP_MODE}`)
 
@@ -28,11 +39,25 @@ const redirectUri = `
 `
 
 onMounted(() => {
+  console.log(route)
+
+  const urlParams = new URLSearchParams(window.location.search)
+  const token = urlParams.get('code')
+  console.log(token)
 })
 
 function lineLogin () {
   const link = `
-  https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=login&scope=profile%20openid&nonce=09876xyz
+  https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${channelId}&redirect_uri=${redirectUri}&state=login&scope=profile%20openid&nonce=09876xyz
+  `
+
+  window.location.href = link
+
+  // userProfile()
+}
+function accessApp () {
+  const link = `
+   https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=${channelId}&redirect_uri=${redirectUri}&state=123abc
   `
 
   window.location.href = link
